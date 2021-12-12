6aa32352208246ea0c5104045ab34f370bfe4486
daszybak <karlo1diidd@gmail.com>
2021-12-13 00:58:51 +0100

    Testni program

---
+++ SimulationTask/ANSIC.prg
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<?AutomationStudio Version=3.0.90.20 SP02?>
+<Program xmlns="http://br-automation.co.at/AS/Program">
+  <Files>
+    <File Description="Cyclic code">SimulationTaskCyclic.c</File>
+    <File Description="Initialization code">SimulationTaskInit.c</File>
+    <File Description="Exit code">SimulationTaskExit.c</File>
+    <File Description="Local data types" Private="true">SimulationTask.typ</File>
+    <File Description="Local variables" Private="true">SimulationTask.var</File>
+  </Files>
+</Program>
---
+++ SimulationTask/SimulationTaskExit.c
@@ -0,0 +1,21 @@
+/********************************************************************
+ * COPYRIGHT --  
+ ********************************************************************
+ * Program: SimulationTask
+ * File: SimulationTaskExit.c
+ * Author: fasch_f
+ * Created: June 27, 2012
+ ********************************************************************
+ * Implementation of program SimulationTask
+ ********************************************************************/
+
+#include <bur/plctypes.h>
+
+#ifdef _DEFAULT_INCLUDES
+	#include <AsDefault.h>
+#endif
+
+void _EXIT SimulationTaskExit(void)
+{
+	/* TODO: Add code here */
+}
